bcb3e5cb659b06da3de5c143b15edc3137d0950c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-03-04 21:34:45 -0800

    lesson 4, add store picker element html

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    // regular comments outside the return...
+    return (
+      <form className="store-selector">
+        { /* JSX comments in brackets - needs to be inside the parent element*/ }
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
